67d7a2da9451c1665504b07f6f59df6d5ee96268
NadineJuliana <nadine.juliana.b@web.de>
2026-02-19 11:20:52 +0100

    Refactor ContactsPageComponent to use dependency injection for ContactsService and initialize contact lists

--- src/app/features/contacts/page/contacts-page/contacts-page.component.ts
+++ src/app/features/contacts/page/contacts-page/contacts-page.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContactsService } from '../../services/contacts.service';
+
 
 @Component({
   selector: 'app-contacts-page',
   imports: [],
   templateUrl: './contacts-page.component.html',
-  styleUrl: './contacts-page.component.scss'
+  styleUrl: './contacts-page.component.scss',
 })
 export class ContactsPageComponent {
+  dbService = inject(ContactsService);
 
+  contactList = this.dbService.contacts;
+  groupedContacts = this.dbService.groupedContacts;
+
+  ngOnInit() {
+    this.dbService.getAllContacts();
+  }
 }
