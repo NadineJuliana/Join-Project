438b86e00e4e71c90f4ae2db35749ac489ff1859
NadineJuliana <nadine.juliana.b@web.de>
2026-02-20 16:48:10 +0100

    Add color property to Contact model and update UI to reflect contact colors

--- src/app/features/contacts/models/contact.model.ts
+++ src/app/features/contacts/models/contact.model.ts
@@ -3,20 +3,36 @@ export class Contact {
   name: string;
   email: string;
   phone: string;
+  color: string;
 
   constructor(data: Partial<Contact>) {
     this.id = data.id ?? 0;
     this.name = data.name ?? '';
     this.email = data.email ?? '';
     this.phone = data.phone ?? '';
+    this.color = data.color ?? this.getRandomColor();
+  }
+
+  getRandomColor(): string {
+    const colors = [
+      '#FF7A00',
+      '#9327FF',
+      '#6E52FF',
+      '#FC71FF',
+      '#FFBB2B',
+      '#1FD7C1',
+      '#462F8A',
+    ];
+    const randomColor = Math.floor(Math.random() * colors.length);
+    return colors[randomColor];
   }
 
   getCleanAddJson() {
     return {
       name: this.name,
       email: this.email,
-      phone: this.phone
+      phone: this.phone,
+      color: this.color,
     };
   }
-
 }
