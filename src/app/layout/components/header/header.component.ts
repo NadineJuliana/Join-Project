677ae996d52318c3b2617081cc879980068a999b
Henry Bergen <henry.bergen@gmx.de>
2026-02-23 12:43:32 +0100

    Refactor header component to enhance profile menu functionality and update styles; correct privacy policy link text case

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isProfileMenuOpen = false;
 
+  toggleProfileMenu(): void {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
 }
