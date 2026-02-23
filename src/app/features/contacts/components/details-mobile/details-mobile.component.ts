663f2d50eba2db65a781020bd07ae4e1d91b78d4
NadineJuliana <nadine.juliana.b@web.de>
2026-02-23 13:34:21 +0100

    Add DetailsMobileComponent with template, styles, and tests

---
+++ src/app/features/contacts/components/details-mobile/details-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-mobile',
+  imports: [],
+  templateUrl: './details-mobile.component.html',
+  styleUrl: './details-mobile.component.scss'
+})
+export class DetailsMobileComponent {
+
+}
