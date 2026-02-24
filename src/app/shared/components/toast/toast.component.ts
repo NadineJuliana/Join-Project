f092c6c5ec723416e37a740cc153f32647ee86b7
NadineJuliana <nadine.juliana.b@web.de>
2026-02-24 18:53:24 +0100

    inject ToastService in ToastComponent

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToastsService } from '../../../core/services/toasts.service';
 
 @Component({
   selector: 'app-toast',
   imports: [],
   templateUrl: './toast.component.html',
-  styleUrl: './toast.component.scss'
+  styleUrl: './toast.component.scss',
 })
 export class ToastComponent {
+  toastService = inject(ToastsService);
 
+  get toastList() {
+    return this.toastService.toastList();
+  }
+
+  //  constructor() {
+  //   this.toastService.showToast({
+  //     message: 'Contact successfully created',
+  //     classname: 'toast__success',
+  //     duration: 999999
+  //   });
 }
