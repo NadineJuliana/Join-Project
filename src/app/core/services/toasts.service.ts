e79fde6ee026deecfbb5b0a6717c524e1d33e103
NadineJuliana <nadine.juliana.b@web.de>
2026-02-24 18:51:55 +0100

    add ToastService interface, show,remove and clear Toast methods

--- src/app/core/services/toasts.service.ts
+++ src/app/core/services/toasts.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+
+export interface Toast {
+  message: string;
+  classname?: string;
+  duration?: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastsService {
+  private readonly toastListSignal = signal<Toast[]>([]);
+  readonly toastList = this.toastListSignal.asReadonly();
 
-  constructor() { }
+  showToast(toast: Toast) {
+    this.toastListSignal.update((list) => [...list, toast]);
+    const duration = toast.duration ?? 1000;
+    setTimeout(() => {
+      this.toastListSignal.update((list) => list.filter((t) => t !== toast));
+    }, duration);
+  }
+
+  removeToast(toast: Toast) {
+    this.toastListSignal.update((list) => list.filter((t) => t !== toast));
+  }
+
+  clearToasts() {
+    this.toastListSignal.set([]);
+  }
 }
