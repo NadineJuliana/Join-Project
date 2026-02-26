046066d8f963d62518446528c2cfdc91cfcf5b4e
NadineJuliana <nadine.juliana.b@web.de>
2026-02-26 14:20:09 +0100

    Add new  Detail Dialog and Task Dialog Components

---
+++ src/app/features/board/components/detail-dialog/detail-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-dialog',
+  imports: [],
+  templateUrl: './detail-dialog.component.html',
+  styleUrl: './detail-dialog.component.scss'
+})
+export class DetailDialogComponent {
+
+}
