701b28be3a42cf246d84dfbf0839dcae77eacc74
NadineJuliana <nadine.juliana.b@web.de>
2026-02-27 15:34:31 +0100

    Add Tasks Service and Task/Subtask Model to Tasks Folder

---
+++ src/app/features/tasks/models/subtask.model.spec.ts
@@ -0,0 +1,7 @@
+import { SubtaskModel } from './subtask.model';
+
+describe('SubtaskModel', () => {
+  it('should create an instance', () => {
+    expect(new SubtaskModel()).toBeTruthy();
+  });
+});
