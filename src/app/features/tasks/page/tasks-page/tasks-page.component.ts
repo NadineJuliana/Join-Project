31604eb7b369489d97f104c4a963c1c99d09dde8
Henry Bergen <henry.bergen@gmx.de>
2026-02-27 10:29:41 +0100

    Add headline and task-form-import to tasks-page

--- src/app/features/tasks/page/tasks-page/tasks-page.component.ts
+++ src/app/features/tasks/page/tasks-page/tasks-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskFormComponent } from '../../components/task-form/task-form.component';
 
 @Component({
   selector: 'app-tasks-page',
-  imports: [],
+  imports: [TaskFormComponent],
   templateUrl: './tasks-page.component.html',
-  styleUrl: './tasks-page.component.scss'
+  styleUrl: './tasks-page.component.scss',
 })
-export class TasksPageComponent {
-
-}
+export class TasksPageComponent {}
