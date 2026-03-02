78c5c3d45985671a14c5caae4b84232b041eed5d
Henry Bergen <henry.bergen@gmx.de>
2026-03-02 03:33:09 +0100

    Add task form layout and styling with input fields for title, description, due date, priority, assignees, category, and subtasks

--- src/app/features/tasks/components/task-form/task-form.component.ts
+++ src/app/features/tasks/components/task-form/task-form.component.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-task-form',
   imports: [],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.scss'
+  styleUrl: './task-form.component.scss',
 })
 export class TaskFormComponent {
+  todayMinDate = this.getTodayMinDate();
 
+  private getTodayMinDate(): string {
+    const now = new Date();
+    const year = now.getFullYear();
+    const monthNumber = now.getMonth() + 1;
+    const dayNumber = now.getDate();
+    const month = monthNumber < 10 ? `0${monthNumber}` : `${monthNumber}`;
+    const day = dayNumber < 10 ? `0${dayNumber}` : `${dayNumber}`;
+    return `${year}-${month}-${day}`;
+  }
 }
