1a3dc8ea97521c2a3fe6f1e1d2da985d100144fb
NadineJuliana <nadine.juliana.b@web.de>
2026-03-02 14:00:16 +0100

    Add SubtaskModel, Interface and getCleanAddJson Method and delete spec.ts

--- src/app/features/tasks/models/subtask.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SubtaskModel } from './subtask.model';
-
-describe('SubtaskModel', () => {
-  it('should create an instance', () => {
-    expect(new SubtaskModel()).toBeTruthy();
-  });
-});
--- src/app/features/tasks/models/subtask.model.ts
+++ src/app/features/tasks/models/subtask.model.ts
@@ -1,2 +1,21 @@
-export class SubtaskModel {
+export class Subtask {
+  id: number;
+  content: string;
+  completed: boolean;
+  task_id: number;
+
+  constructor(data: Partial<Subtask>) {
+    this.id = data.id ?? 0;
+    this.content = data.content ?? '';
+    this.completed = data.completed ?? false;
+    this.task_id = data.task_id ?? 0;
+  }
+
+  getCleanAddJson() {
+    return {
+      content: this.content,
+      completed: this.completed,
+      task_id: this.task_id,
+    };
+  }
 }
