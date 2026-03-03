0c1981b262652f86aa6a2675a61cbb2fd5590a3b
NadineJuliana <nadine.juliana.b@web.de>
2026-03-03 15:25:20 +0100

    Refactor Cdk Event on BoardPage

--- src/app/features/board/page/board-page/board-page.component.ts
+++ src/app/features/board/page/board-page/board-page.component.ts
@@ -3,21 +3,27 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TasksService } from '../../../tasks/services/tasks.service';
 import { ContactsService } from '../../../contacts/services/contacts.service';
-import { Task } from '../../../tasks/models/task.model';
+import { Task, TaskStatus } from '../../../tasks/models/task.model';
 import { InitialsPipe } from '../../../../shared/pipes/initials.pipe';
 import { EllipsisPipe } from '../../../../shared/pipes/ellipsis.pipe';
 import { CapitalizePipe } from '../../../../shared/pipes/capitalize.pipe';
 
 // Interface - Definiert die Struktur einer Spalte
 export interface BoardColumn {
-  id: string;
+  id: TaskStatus;
   title: string;
   tasks: Task[];
 }
 
 @Component({
   selector: 'app-board-page',
-  imports: [CommonModule, DragDropModule, InitialsPipe, EllipsisPipe, CapitalizePipe],
+  imports: [
+    CommonModule,
+    DragDropModule,
+    InitialsPipe,
+    EllipsisPipe,
+    CapitalizePipe,
+  ],
   templateUrl: './board-page.component.html',
   styleUrl: './board-page.component.scss',
 })
@@ -50,39 +56,23 @@ export class BoardPageComponent {
     ];
   }
 
-  // - Die Drop-Handler Methode
   drop(event: CdkDragDrop<Task[]>) {
-    // event.previousContainer = die Spalte, VON der die Task kommt
-    // event.container = die Spalte, IN die die Task geht
-
-    // - Wenn die Task in die gleiche Spalte geht
-    if (event.previousContainer === event.container) {
-      // Nur die Reihenfolge ändern (z.B. Task nach oben/unten verschieben)
-      const items = event.container.data;
-      const item = items[event.previousIndex];
-      items.splice(event.previousIndex, 1);
-      items.splice(event.currentIndex, 0, item);
-    }
-    // - Wenn die Task in eine andere Spalte geht
-    else {
-      // Task aus alter Spalte entfernen
-      const item = event.previousContainer.data[event.previousIndex];
-      event.previousContainer.data.splice(event.previousIndex, 1);
-
-      // Task in neue Spalte einfügen
-      event.container.data.splice(event.currentIndex, 0, item);
-    }
-
-    // Border nach dem Drop ausblenden
-    this.activeDropListId = null;
+    const task = event.item.data as Task;
+    const newStatus = event.container.id as TaskStatus;
+    event.previousContainer.data.splice(event.previousIndex, 1);
+    event.container.data.splice(event.currentIndex, 0, task);
+    task.status = newStatus;
+    this.dbTaskService.moveTaskAndReorder(
+      task,
+      event.container.data,
+      newStatus,
+    );
   }
 
-  // Wird aufgerufen, wenn eine Task über eine Spalte hovest
   onDropListEntered(columnId: string): void {
     this.activeDropListId = columnId;
   }
 
-  // Wird aufgerufen, wenn eine Task die Spalte verlässt
   onDropListExited(columnId: string): void {
     this.activeDropListId = null;
   }
