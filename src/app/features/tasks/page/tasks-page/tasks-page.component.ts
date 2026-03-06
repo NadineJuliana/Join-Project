bcd91765ce9047c59947b862339a17d7932b3569
NadineJuliana <nadine.juliana.b@web.de>
2026-03-06 09:50:09 +0100

    Add status Signal and onInit TaskStatus to TaskPage Component

--- src/app/features/tasks/page/tasks-page/tasks-page.component.ts
+++ src/app/features/tasks/page/tasks-page/tasks-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TaskFormComponent } from '../../components/task-form/task-form.component';
 import { ToastComponent } from '../../../../shared/components/toast/toast.component';
+import { ActivatedRoute } from '@angular/router';
+import { TaskStatus } from '../../models/task.model';
 
 @Component({
   selector: 'app-tasks-page',
@@ -8,4 +10,16 @@ import { ToastComponent } from '../../../../shared/components/toast/toast.compon
   templateUrl: './tasks-page.component.html',
   styleUrl: './tasks-page.component.scss',
 })
-export class TasksPageComponent {}
+export class TasksPageComponent {
+  private route = inject(ActivatedRoute);
+  status = signal<TaskStatus>('to-do');
+
+  ngOnInit() {
+    const statusParam = this.route.snapshot.queryParamMap.get(
+      'status',
+    ) as TaskStatus;
+    if (statusParam) {
+      this.status.set(statusParam);
+    }
+  }
+}
