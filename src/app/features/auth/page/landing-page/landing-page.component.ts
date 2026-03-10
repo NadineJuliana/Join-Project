d85e473877c14a58d1a35ee3f432094df46375fd
Henry Bergen <henry.bergen@gmx.de>
2026-03-10 11:59:30 +0100

    Add login form to landing page and update styles for animation

--- src/app/features/auth/page/landing-page/landing-page.component.ts
+++ src/app/features/auth/page/landing-page/landing-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [LoginFormComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
