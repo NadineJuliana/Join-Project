dcfedfa3c3a9e1bc3a08678aff71d0cc22f3bba8
Vladislav P <perjuvladislav@gmail.com>
2026-03-10 18:43:19 +0100

    Refactor summary page component to integrate TasksService and compute task counts

--- src/app/features/summary/page/summary-page/summary-page.component.ts
+++ src/app/features/summary/page/summary-page/summary-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TasksService } from '../../../tasks/services/tasks.service';
 
 @Component({
   selector: 'app-summary-page',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './summary-page.component.scss'
 })
 export class SummaryPageComponent {
+  tasksService = inject(TasksService);
 
+  async ngOnInit(){
+    this.tasksService.initRealtime();
+    await this.tasksService.initialize();
+  }
+
+  toDoCount = computed(() => this.tasksService.toDoTasks().length);
+  doneCount = computed(() => this.tasksService.doneTasks().length);
+  urgentCount = computed(() => this.tasksService.urgentTasks().length);
+  boardCount = computed(() => this.tasksService.tasks().length);
+  inProgressCount = computed(() => this.tasksService.inProgressTasks().length);
+  awaitFeedbackCount = computed(() => this.tasksService.awaitFeedbackTasks().length);
 }
