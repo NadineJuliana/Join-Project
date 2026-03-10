f745468a45bf90155ef750e1633c9a65fd359b0d
NadineJuliana <nadine.juliana.b@web.de>
2026-03-10 15:19:34 +0100

    Add authGuard Method for routing to AuthGuard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { SupabaseService } from '../services/supabase.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async () => {
+  const supabaseClient = inject(SupabaseService).getSupabaseClient();
+  const router = inject(Router);
+  const { data } = await supabaseClient.auth.getSession();
+  if (!data.session) {
+    router.navigate(['/login']);
+    return false;
+  }
   return true;
 };
