010f2e5632762bdef0615441d9a9dc47de874121
NadineJuliana <nadine.juliana.b@web.de>
2026-03-11 13:29:25 +0100

    Add Auth Service and redirect to Login on PrivacyPolicy and LegalNotice Components

--- src/app/features/legalnotice/legalnotice-page/legalnotice-page.component.ts
+++ src/app/features/legalnotice/legalnotice-page/legalnotice-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-legalnotice-page',
   imports: [],
   templateUrl: './legalnotice-page.component.html',
-  styleUrl: './legalnotice-page.component.scss'
+  styleUrl: './legalnotice-page.component.scss',
 })
-export class LegalnoticePageComponent {
 
+export class LegalnoticePageComponent implements OnInit{
+  isLoggedIn = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  async ngOnInit() {
+    this.isLoggedIn = await this.auth.isLoggedIn();
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/login']);
+    }
+  }
 }
--- src/app/features/privacy/privacypolicy-page/privacypolicy-page.component.ts
+++ src/app/features/privacy/privacypolicy-page/privacypolicy-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacypolicy-page',
   imports: [],
   templateUrl: './privacypolicy-page.component.html',
-  styleUrl: './privacypolicy-page.component.scss'
+  styleUrl: './privacypolicy-page.component.scss',
 })
-export class PrivacypolicyPageComponent {
 
+export class PrivacypolicyPageComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  async ngOnInit() {
+    this.isLoggedIn = await this.auth.isLoggedIn();
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/login']);
+    }
+  }
 }
