6edda815f643d3df40122e01a14a551c0e9e31e4
NadineJuliana <nadine.juliana.b@web.de>
2026-03-11 13:51:14 +0100

    Add InitialPipe for profile Icon and isGuest/currentUser Methods on Header Component

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -7,15 +7,18 @@ import {
 } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../features/auth/services/auth.service';
+import { ContactsService } from '../../../features/contacts/services/contacts.service';
+import { InitialsPipe } from '../../../shared/pipes/initials.pipe';
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, InitialsPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private contactsService = inject(ContactsService);
   private authService = inject(AuthService);
   private router = inject(Router);
   isProfileMenuOpen = false;
@@ -40,5 +43,13 @@ export class HeaderComponent {
   async logout() {
     await this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  get currentUser() {
+    return this.contactsService.currentUserContact;
+  }
+
+  get isGuest() {
+    return !!localStorage.getItem('guest');
   }
 }
