13f4c6c687671a172a1a868ad80a6b236d2a47a6
NadineJuliana <nadine.juliana.b@web.de>
2026-03-11 13:31:06 +0100

    Add loginAsGuest Method on LoginForm Component

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -8,6 +10,8 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
   private formBuilder = inject(FormBuilder);
   authErrorMessage = '';
   showPassword = false;
@@ -36,13 +40,21 @@ export class LoginFormComponent {
     this.isPasswordFocused = false;
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
       return;
     }
-
     this.authErrorMessage = '';
+    const { email, password } = this.form.getRawValue();
+    await this.authService.login(email, password);
+    this.router.navigate(['/summary']);
     // Auth submission folgt nach Implementierung des AuthService bzw. Erstellung des Signup-Forms
+  }
+
+  async loginAsGuest() {
+    this.authErrorMessage = '';
+    await this.authService.loginAsGuest();
+    this.router.navigate(['/summary']);
   }
 }
