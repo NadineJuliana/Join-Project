6994bfcf7b9b060c51f3f81b05b7967f5b567e40
NadineJuliana <nadine.juliana.b@web.de>
2026-03-12 13:11:42 +0100

    Add style, html and goBack and highlightJoin Method on LegalNotice Page Component and add media queries

--- src/app/features/legalnotice/legalnotice-page/legalnotice-page.component.ts
+++ src/app/features/legalnotice/legalnotice-page/legalnotice-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/services/auth.service';
 import { Router } from '@angular/router';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Location } from '@angular/common';
+import { LEGALNOTICE_CONTENT } from '../components/legalnotice-content';
 
 @Component({
   selector: 'app-legalnotice-page',
@@ -8,13 +11,15 @@ import { Router } from '@angular/router';
   templateUrl: './legalnotice-page.component.html',
   styleUrl: './legalnotice-page.component.scss',
 })
-
-export class LegalnoticePageComponent implements OnInit{
+export class LegalnoticePageComponent implements OnInit {
   isLoggedIn = false;
+  content = LEGALNOTICE_CONTENT;
 
   constructor(
     private auth: AuthService,
     private router: Router,
+    private sanitizer: DomSanitizer,
+    private location: Location,
   ) {}
 
   async ngOnInit() {
@@ -22,5 +27,20 @@ export class LegalnoticePageComponent implements OnInit{
     if (!this.isLoggedIn) {
       this.router.navigate(['/login']);
     }
+  }
+
+  highlightJoin(text: string): SafeHtml {
+    if (!text) return '';
+    let html = text;
+    html = text.replace(/Join/g, `<span class="join-highlight">Join</span>`);
+    html = html.replace(
+      /Developer Akademie GmbH/g,
+      `<span class="developer-highlight">Developer Akademie GmbH</span>`,
+    );
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
