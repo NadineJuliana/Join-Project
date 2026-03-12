d8b5750da741c15318b3e469edbac45fb5eb606f
NadineJuliana <nadine.juliana.b@web.de>
2026-03-12 13:09:58 +0100

    Add LEgalnotice Content Typescript Json

---
+++ src/app/features/legalnotice/components/legalnotice-content.ts
@@ -0,0 +1,47 @@
+export const LEGALNOTICE_CONTENT = {
+  headline: 'Legal Notice',
+  sections: [
+    {
+      title: 'Imprint',
+      address: [
+        'Nadine Bauer',
+        'Musterweg 1',
+        '12345 Musterhausen',
+      ],
+    },
+    {
+    steps: [
+      {
+        title: 'Exploring the board',
+        text: 'test@example.de',
+      },
+      {
+        title: 'Acceptance of terms',
+        text: 'By accessing and using Join(Product), you acknowledge and agree to the folowing terms and conditions, and any policies, guidelines, or amendments thereto that may presentet to you from time to time. We, the listed students may update or change the terms and conditions from time to time without notice.',
+      },
+      {
+        title: 'Scope and owenersship of the product',
+        text: 'Join has been developed as part of a student group project in a web developmet bootcamp at the Developer Akademie GmbH. It has an educational purpose and is not intended for extensive personal & business usage. As such, we cannot guarantee consistent availablillity, reliabillity, accuracy, or any other aspect of quality regarding this Product. The Design of Join is owned by the Developer Akademie GmbH. Unauthorized use, reproduction, modification, distribution, or replication of the design is strictly prohibited. ',
+      },
+      {
+        title: 'Proprietary rigths',
+        text: 'Aside from the design owned by the Developer Akademie GmbH, we, the list students, retain all proprietary rigths in Join, including any associated copyright material, trademarks, and other proprietary information.',
+      },
+      {
+        title: 'Use of the product',
+        text: 'Join is intended to be used for lawful purpose only, in accordance with all applicable laws and regulations. Any use of Join for illegal activities, or harass, harm, threaten, or intimidate another person, is strictly prohibited. You are solely responisble for your interactions with other users of Join.',
+      },
+      {
+        title: 'Disclamier of warranties and limitation of liability',
+        text: 'Join is provided "as is" without warrenty of any kind, wether express or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, and non-infringment. In no event will we, the listed students, or the Developer Akademie GmbH, be liable for any direct indirect, incidental, special, consequential or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses, even if we have been advised of possibility of such damages, arising out of or in conecction with the use or performance of Join.',
+      },
+      {
+        title: 'Indemnity',
+        text: 'You agree to imdenify, defend and hold harmless us, the list students, the Developer Akademie GmbH, and our affiliates, partners, officers, directors, agents, and employees, from and against any claim, demand, loss, damage, cost or lianbility (including resonable legal fees) arising out of or relating to your use of Koin and/ or your breach of thsis Legal Notice.',
+      },
+    ],
+    },
+  ],
+  close: 'For any questions or notices, please contact us at <a href="mailto:your-contact-email@example.com">projects.developerakademie@web.de</a>.',
+  date: 'Date: March 12, 2026',
+};
