dd38d83f324758d02447992ac716e48be0f992c8
NadineJuliana <nadine.juliana.b@web.de>
2026-03-12 20:00:13 +0100

    Add Responsive/Style for Sidebar without Login

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../features/auth/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,4 +10,11 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent implements OnInit {
+  private authService = inject(AuthService);
+  isLoggedIn = false;
+
+  async ngOnInit() {
+    this.isLoggedIn = await this.authService.isLoggedIn();
+  }
+}
