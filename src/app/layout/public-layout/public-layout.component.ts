7c8b2b2b824e81b1c185e3a20e29e0b6a14070d6
NadineJuliana <nadine.juliana.b@web.de>
2026-03-12 14:48:13 +0100

    Import Header, Sidebar and Routeroutlet and add style on Public Layout Component

--- src/app/layout/public-layout/public-layout.component.ts
+++ src/app/layout/public-layout/public-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, RouterOutlet],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.scss'
 })
