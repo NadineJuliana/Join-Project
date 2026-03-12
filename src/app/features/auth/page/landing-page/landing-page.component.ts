3a41596d5802390d0ded8d1fe1d920595f9cfb4f
Henry Bergen <henry.bergen@gmx.de>
2026-03-12 09:40:58 +0100

    Add legal links to landing page and update styles for legal section

--- src/app/features/auth/page/landing-page/landing-page.component.ts
+++ src/app/features/auth/page/landing-page/landing-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
 import { SignupFormComponent } from '../../components/signup-form/signup-form.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LoginFormComponent, SignupFormComponent],
+  imports: [LoginFormComponent, SignupFormComponent, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
