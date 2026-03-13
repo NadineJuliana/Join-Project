35d9ab9c4d140a2bdfbac407bef81a59012500e1
NadineJuliana <nadine.juliana.b@web.de>
2026-03-13 12:05:05 +0100

    Add Arrow Back on PrivacyPolicy Page Component

--- src/app/features/privacy/privacypolicy-page/privacypolicy-page.component.ts
+++ src/app/features/privacy/privacypolicy-page/privacypolicy-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-privacypolicy-page',
@@ -6,4 +9,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './privacypolicy-page.component.html',
   styleUrl: './privacypolicy-page.component.scss',
 })
-export class PrivacypolicyPageComponent {}
+export class PrivacypolicyPageComponent {
+  isLoggedIn = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private location: Location,
+  ) {}
+
+  async ngOnInit() {
+    this.isLoggedIn = await this.authService.isLoggedIn();
+  }
+
+  goBack() {
+    if (window.history.length > 1) {
+      this.location.back();
+    } else {
+      this.router.navigate(['/summary']);
+    }
+  }
+}
